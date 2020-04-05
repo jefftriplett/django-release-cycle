7102bb558abab8758786e02cb58c90cbe3880e5e
Jeff Triplett <jeff.triplett@gmail.com>
2020-04-05 18:33:41 -0500

    :bug: Fixes date issue

--- script.js
+++ script.js
@@ -178,79 +178,87 @@ function drawChart() {
     ]);
   }
 
+  // height per horizontal track
+  var trackHeight = 30;
+  // buffer for container height
+  var bufferForHeight = 50;
+  // height of entire container
+  var containerHeight = (trackHeight * releases.length) + bufferForHeight;
+
   var options = {
-    height: 400,
+    height: containerHeight,
     gantt: {
-      trackHeight: 30,
+      trackHeight: trackHeight,
       palette: [
         {
           // Red
           color: "#db4437",
           dark: "#a52714",
-          light: "#f4c7c3",
+          light: "#f4c7c3"
         },
         {
           // Yellow
           color: "#f2a600",
           dark: "#ee8100",
-          light: "#fce8b2",
+          light: "#fce8b2"
         },
         {
           // Green
           color: "#0f9d58",
           dark: "#0b8043",
-          light: "#b7e1cd",
+          light: "#b7e1cd"
         },
         {
           // Blue
           color: "#5e97f6",
           dark: "#2a56c6",
-          light: "#c6dafc",
+          light: "#c6dafc"
         },
         {
           color: "#ab47bc",
           dark: "#6a1b9a",
-          light: "#e1bee7",
+          light: "#e1bee7"
         },
         {
           color: "#00acc1",
           dark: "#00838f",
-          light: "#b2ebf2",
+          light: "#b2ebf2"
         },
         {
           color: "#ff7043",
           dark: "#e64a19",
-          light: "#ffccbc",
+          light: "#ffccbc"
         },
         {
           color: "#9e9d24",
           dark: "#827717",
-          light: "#f0f4c3",
+          light: "#f0f4c3"
         },
         {
           color: "#5c6bc0",
           dark: "#3949ab",
-          light: "#c5cae9",
+          light: "#c5cae9"
         },
         {
           color: "#f06292",
           dark: "#e91e63",
-          light: "#f8bbd0",
+          light: "#f8bbd0"
         },
         {
           color: "#00796b",
           dark: "#004d40",
-          light: "#b2dfdb",
+          light: "#b2dfdb"
         },
         {
           color: "#c2185b",
           dark: "#880e4f",
-          light: "#f48fb1",
-        },
-      ],
-    },
+          light: "#f48fb1"
+        }
+      ]
+    }
   };
 
+  var container = document.getElementById("chart_div");
   var chart = new google.visualization.Gantt(
     container
   );
